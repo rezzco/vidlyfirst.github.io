546d169c9759e4b2922ee9cc86fe69a4c7aff37f
rezzco <47239458+rezzco@users.noreply.github.com>
2019-02-04 23:29:34 -0500

    an error occured

---
+++ Scripts/FormJavaScripts.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    var tBox = document.getElementById("birthDate");
+    var textvalue = tBox.value;
+    var update = "";
+    tBox.onmouseenter = function () {
+            this.value = update;
+        
+       
+    };
+    tBox.onmouseleave = function () {
+        if (this.value=="") {
+            this.value = textvalue;
+        } else {
+            update = this.value;
+        }
+        
+    };
+    
+}());
